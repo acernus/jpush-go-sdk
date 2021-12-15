76a400f90608c2f047efd317ea8447f6c4dae54c
acernus <desirelch123@gmail.com>
2021-12-15 15:01:04 +0800

    fix: change android intent struct

--- options.go
+++ options.go
@@ -1,12 +1,5 @@
 package jpush
 
-type ApnsProduction string
-
-const (
-	Production ApnsProduction = "True"
-	Test       ApnsProduction = "False"
-)
-
 type Options struct {
-	ApnsProduction string `json:"apns_production,omitempty"`
+	ApnsProduction bool `json:"apns_production"` // 不指定默认为 true
 }
