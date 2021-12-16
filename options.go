533eec6fe6db5f129c6ad7cb058aef0a5a887c94
acernus <desirelch123@gmail.com>
2021-12-16 17:31:05 +0800

    fix: add third channel

--- options.go
+++ options.go
@@ -1,5 +1,53 @@
 package jpush
 
+
+const (
+	ThirdPartyChannelHuawei = "huawei"
+	ThirdPartyChannelXiaomi = "xiaomi"
+	ThirdPartyChannelMeizu  = "meizu"
+	ThirdPartyChannelOppo   = "oppo"
+	ThirdPartyChannelVivo   = "vivo"
+	ThirdPartyChannelAsus   = "asus"
+
+	DistributionJpush         = "jpush"
+	DistributionOspush        = "ospush"
+	DistributionSecondaryPush = "secondary_push"
+	DistributionFirstOspush   = "first_ospush"
+)
+
 type Options struct {
-	ThirdPartyChannel map[string]inteface{} `json:"third_party_channel,omitempty"` // 生产环境为 true
+	ThirdPartyChannel map[string]*ThirdPartyChannel `json:"third_party_channel,omitempty"`
+}
+
+type ThirdPartyChannel struct {
+	Distribution          string                 `json:"distribution,omitempty"`
+	DistributionFcm       string                 `json:"distribution_fcm,omitempty"`
+	DistributionCustomize string                 `json:"distribution_customize,omitempty"`
+	ChannelID             string                 `json:"channel_id,omitempty"`
+	SkipQuota             bool                   `json:"skip_quota,omitempty"`
+	Classification        int                    `json:"classification,omitempty"`
+	PushMode              int                    `json:"push_mode,omitempty"`
+	Importance            string                 `json:"importance,omitempty"`
+	Urgency               string                 `json:"urgency,omitempty"`
+	Category              string                 `json:"category,omitempty"`
+	LargeIcon             string                 `json:"large_icon,omitempty"`
+	SmallIconUri          string                 `json:"small_icon_uri,omitempty"`
+	SmallIconColor        string                 `json:"small_icon_color,omitempty"`
+	Style                 int                    `json:"style,omitempty"`
+	BigText               string                 `json:"big_text,omitempty"`
+	Inbox                 map[string]interface{} `json:"inbox,omitempty"`
+	BigPicPath            string                 `json:"big_pic_path,omitempty"`
+	OnlyUseVendorStyle    bool                   `json:"only_use_vendor_style,omitempty"`
+}
+
+func NewOptions() *Options {
+	return &Options{}
+}
+
+func (opt *Options) FillThirdPartyChannel(channelName string, param *ThirdPartyChannel) *Options {
+	if opt.ThirdPartyChannel == nil {
+		opt.ThirdPartyChannel = make(map[string]*ThirdPartyChannel)
+	}
+	opt.ThirdPartyChannel[channelName] = param
+	return opt
 }
